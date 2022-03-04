2bf2184db0c3f3cedfd0d26255302a607473e58b
PedroCosta@PenguinStation <pedromrodriguescosta@gmail.com>
2022-03-04 11:29:13 +0000

    ok go

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,36 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        ArrayList<String> words = null;
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("resources/words.txt"));
+            String line = "";
+            words = new ArrayList<>();
+            while ((line = reader.readLine()) != null) {
+                words.add(line);
+            }
+
+            System.out.println(words.size());
+            reader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
         String guess = "";
-        String word = "picha";
+        String word = words.get((int)(Math.random() * words.size()));
+        word = words.get(3);
         int letters = word.length();
         String[] lettersGuessed = new String[word.length()];
         boolean found = false;
@@ -16,13 +41,32 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         for (int i = 0; i < lettersGuessed.length; i++) {
-            lettersGuessed[i] = "_";
-            System.out.print(lettersGuessed[i] + " ");
+            if (word.charAt(i) == ' ') {
+                lettersGuessed[i] = " ";
+                letters--;
+            } else if (word.charAt(i) == '-'){
+                lettersGuessed[i] = "-";
+                letters--;
+            } else {
+                lettersGuessed[i] = "_";
+            }
         }
+
+        System.out.println();
+
 
         while (letters != 0) {
             found = false;
+
+            for (int i = 0; i < lettersGuessed.length; i++) {
+                System.out.print(lettersGuessed[i] + " ");
+            }
+            System.out.println();
+
             guess = scanner.nextLine();
+            if (guess.equals(null) || guess.equals("")) {
+                continue;
+            }
 
             if (guess.length() > 1) {
                 if (guess.equals(word)) {
@@ -41,19 +85,17 @@ public class Main {
                 }
 
                 if (!found) {
-                    lettersGuessed[word.indexOf(guess)] = guess;
-                    letters--;
+                    for (int i = 0; i < lettersGuessed.length; i++) {
+                        if (word.charAt(i) == guess.charAt(0)) {
+                            lettersGuessed[i] = guess;
+                            letters--;
+                        }
+                    }
                 }
-
-                for (int i = 0; i < lettersGuessed.length; i++) {
-                    System.out.print(lettersGuessed[i] + " ");
-                }
-
-                System.out.println();
             }
         }
 
-        System.out.println("YOU WIN");
+        System.out.println(word);
     }
 
 
