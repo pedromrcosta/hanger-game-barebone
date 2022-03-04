0c168da74900a48fc38e9b0584ad77347d27ae08
PedroCosta@PenguinStation <pedromrodriguescosta@gmail.com>
2022-03-04 11:37:59 +0000

    ok go

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,80 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList<String> words = null;
+        ArrayList<String> words = readFile();
 
+        boolean found = false;
+        String guess = "";
+        String wordToGuess = words.get((int)(Math.random() * words.size()));
+        String[] lettersGuessed = new String[wordToGuess.length()];
+        int letters = wordToGuess.length();
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < lettersGuessed.length; i++) {
+            if (wordToGuess.charAt(i) == ' ') {
+                lettersGuessed[i] = " ";
+                letters--;
+            } else if (wordToGuess.charAt(i) == '-'){
+                lettersGuessed[i] = "-";
+                letters--;
+            } else {
+                lettersGuessed[i] = "_";
+            }
+        }
+
+        while (letters != 0) {
+            found = false;
+
+            for (int i = 0; i < lettersGuessed.length; i++) {
+                System.out.print(lettersGuessed[i] + " ");
+            }
+            System.out.println();
+
+            guess = scanner.nextLine();
+            if (guess.equals(null) || guess.equals("")) {
+                continue;
+            }
+
+            // Se escreveu + que uma letra, está a tentar adivinhar a palavra
+            if (guess.length() > 1) {
+                if (guess.equals(wordToGuess)) {
+                    break;
+                }
+                System.out.println("Failed to guess word");
+                continue;
+            }
+
+            // Se for só uma letra, vai verificar se existe e se ainda não acertou
+            if (wordToGuess.indexOf(guess) > -1) {
+                // verificação se já encontrou letra
+                for (int i = 0; i < lettersGuessed.length; i++) {
+                    if (lettersGuessed[i].equals(guess)) {
+                        System.err.println("You have already found that letter !");
+                        found = true;
+                        break;
+                    }
+                }
+
+                // se acertou a letra e ainda não a tinha descoberto
+                // então faz cenas
+                if (!found) {
+                    for (int i = 0; i < lettersGuessed.length; i++) {
+                        if (wordToGuess.charAt(i) == guess.charAt(0)) {
+                            lettersGuessed[i] = guess;
+                            letters--;
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println(wordToGuess);
+    }
+
+    public static ArrayList<String> readFile() {
+        ArrayList<String> words = null;
         try {
             BufferedReader reader = new BufferedReader(new FileReader("resources/words.txt"));
             String line = "";
@@ -27,75 +99,7 @@ public class Main {
             e.printStackTrace();
         }
 
-
-        String guess = "";
-        String word = words.get((int)(Math.random() * words.size()));
-        word = words.get(3);
-        int letters = word.length();
-        String[] lettersGuessed = new String[word.length()];
-        boolean found = false;
-        //P I C H A
-        //_ _ _ _ _
-        //_ _ _ H _
-
-        Scanner scanner = new Scanner(System.in);
-
-        for (int i = 0; i < lettersGuessed.length; i++) {
-            if (word.charAt(i) == ' ') {
-                lettersGuessed[i] = " ";
-                letters--;
-            } else if (word.charAt(i) == '-'){
-                lettersGuessed[i] = "-";
-                letters--;
-            } else {
-                lettersGuessed[i] = "_";
-            }
-        }
-
-        System.out.println();
-
-
-        while (letters != 0) {
-            found = false;
-
-            for (int i = 0; i < lettersGuessed.length; i++) {
-                System.out.print(lettersGuessed[i] + " ");
-            }
-            System.out.println();
-
-            guess = scanner.nextLine();
-            if (guess.equals(null) || guess.equals("")) {
-                continue;
-            }
-
-            if (guess.length() > 1) {
-                if (guess.equals(word)) {
-                    break;
-                }
-                System.out.println("Failed to guess word");
-                continue;
-            }
-
-            if (word.indexOf(guess) > -1) {
-                for (int i = 0; i < lettersGuessed.length; i++) {
-                    if (lettersGuessed[i].equals(guess)) {
-                        System.out.println("JA ENCONTRASTE ESSA LETRA BURRO");
-                        found = true;
-                    }
-                }
-
-                if (!found) {
-                    for (int i = 0; i < lettersGuessed.length; i++) {
-                        if (word.charAt(i) == guess.charAt(0)) {
-                            lettersGuessed[i] = guess;
-                            letters--;
-                        }
-                    }
-                }
-            }
-        }
-
-        System.out.println(word);
+        return words;
     }
 
 
